d5f89835b62a9a586336693388a64c2ef3f63064
lucero202 <64439800+lucero202@users.noreply.github.com>
2021-11-04 15:01:49 -0500

    Update Control_Versiones.java

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -16,6 +16,7 @@ public class Control_Versiones {
      */
     public static void main(String[] args) {
         System.out.println("Version 1");
+        System.out.println("Version 2");
     }
     
 }
