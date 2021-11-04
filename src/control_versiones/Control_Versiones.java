d0a3c6f025b3bc959df316e9623e2126daded8a7
Lucero <Lucero@DESKTOP-U2IK6CD>
2021-11-04 14:59:41 -0500

    Carga inicial

---
+++ src/control_versiones/Control_Versiones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control_versiones;
+
+/**
+ *
+ * @author Lucero
+ */
+public class Control_Versiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Version 1");
+    }
+    
+}
